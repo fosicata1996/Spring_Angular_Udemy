a403b2b982d62a2dfae6aa2fdf94f8a428ae808a
fosicata1996 <fosalaucatalin@gmail.com>
2021-01-20 12:40:01 +0200

    01. TypeScript - Classes - Access modifiers

--- typescript/04-classes/Customer.ts
+++ typescript/04-classes/Customer.ts
@@ -1,12 +1,32 @@
 class Customer
 {
-    firstName: string;
-    lastName: string;
+    private _firstName: string;
+    private _lastName: string;
 
     constructor(firstName: string, lastName: string)
     {
-        this.firstName = firstName;
-        this.lastName = lastName;
+        this._firstName = firstName;
+        this._lastName = lastName;
+    }
+
+    public get firstName(): string
+    {
+        return this._firstName;
+    }
+
+    public set firstName(value: string)
+    {
+        this._firstName = value;
+    }
+
+    public get lastName(): string
+    {
+        return this._lastName;
+    }
+
+    public set lastName(value: string)
+    {
+        this._lastName = value;
     }
 }
 
