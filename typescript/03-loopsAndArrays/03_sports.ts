41b99f4795f2d87cfd55be48a6c028fb69e03215
fosicata1996 <fosalaucatalin@gmail.com>
2021-01-20 12:04:57 +0200

    01. TypeScript - Loops & Arrays

---
+++ typescript/03-loopsAndArrays/03_sports.ts
@@ -0,0 +1,13 @@
+let sportsOne: string[] = ["Golf", "Cricket", "Tennis", "Swimming"];
+
+for (let tempSport of sportsOne)
+{
+    if (tempSport == "Cricket")
+    {
+        console.log(tempSport + " << My Favorite!");
+    }
+    else
+    {
+        console.log(tempSport);
+    }
+}
