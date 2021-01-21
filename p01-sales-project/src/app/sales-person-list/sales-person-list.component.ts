76482677fab0763445e86cee703d65263bf64398
fosicata1996 <fosalaucatalin@gmail.com>
2021-01-21 10:27:47 +0200

    02. Angular - p01-sales-project - Display array data

--- p01-sales-project/src/app/sales-person-list/sales-person-list.component.ts
+++ p01-sales-project/src/app/sales-person-list/sales-person-list.component.ts
@@ -1,4 +1,5 @@
 import {Component, OnInit} from '@angular/core';
+import {SalesPerson} from "./sales-person";
 
 @Component({
   selector: 'app-sales-person-list',
@@ -7,6 +8,13 @@ import {Component, OnInit} from '@angular/core';
 })
 export class SalesPersonListComponent implements OnInit
 {
+
+  salesPersonList: SalesPerson[] = [
+    new SalesPerson("Anup", "Kumar", "anup.kumar@luv2code.com", 5000),
+    new SalesPerson("John", "Doe", "john.doe@luv2code.com", 4000),
+    new SalesPerson("Claire", "Murphy", "claire.murphy@luv2code.com", 9000),
+    new SalesPerson("Mai", "Truong", "mai.truong@luv2code.com", 6000)
+  ];
 
   constructor()
   {
