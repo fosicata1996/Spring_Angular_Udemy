dcff56cbea5d54914ca9bcc32e6dd39252234460
fosicata1996 <fosalaucatalin@gmail.com>
2021-01-25 18:25:57 +0200

    03. Angular & Spring - p02-spring-boot-ecommerce

---
+++ p02-spring-boot-ecommerce/src/test/java/com/luv2code/ecommerce/SpringBootEcommerceApplicationTests.java
@@ -0,0 +1,15 @@
+package com.luv2code.ecommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootEcommerceApplicationTests
+{
+	
+	@Test
+	void contextLoads()
+	{
+	}
+	
+}
