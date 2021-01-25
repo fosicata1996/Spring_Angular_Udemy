de607c3266f452c7d838ad15e4f34f65b1f1370d
fosicata1996 <fosalaucatalin@gmail.com>
2021-01-25 23:48:30 +0200

    06. Angular & Spring - Routing - Display products by category - Load dynamically

--- p03-angular-ecommerce/src/app/services/product.service.ts
+++ p03-angular-ecommerce/src/app/services/product.service.ts
@@ -3,6 +3,7 @@ import {HttpClient} from "@angular/common/http";
 import {Product} from "../common/product";
 import {Observable} from "rxjs";
 import {map} from 'rxjs/operators';
+import {ProductCategory} from "../common/product-category";
 
 @Injectable({
   providedIn: 'root'
@@ -11,6 +12,7 @@ export class ProductService
 {
 
   private baseUrl = 'http://localhost:8081/api/products';
+  private categoryUrl = 'http://localhost:8081/api/product-category';
 
   constructor(private httpClient: HttpClient)
   {
@@ -21,15 +23,29 @@ export class ProductService
     // need to build URL based on category id
     const searchUrl = `${this.baseUrl}/search/findByCategoryId?id=${theCategoryId}`;
 
-    return this.httpClient.get<GetResponse>(searchUrl).pipe(
+    return this.httpClient.get<GetResponseProducts>(searchUrl).pipe(
       map(response => response._embedded.products)
+    );
+  }
+
+  getProductCategories(): Observable<ProductCategory[]>
+  {
+    return this.httpClient.get<GetResponseProductCategory>(this.categoryUrl).pipe(
+      map(response => response._embedded.productCategory)
     );
   }
 }
 
-interface GetResponse
+interface GetResponseProducts
 {
   _embedded: {
     products: Product[];
+  }
+}
+
+interface GetResponseProductCategory
+{
+  _embedded: {
+    productCategory: ProductCategory[];
   }
 }
