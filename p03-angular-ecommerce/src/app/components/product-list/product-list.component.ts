0d8fc83f40527a4411c1d5dc1d0f18681f8e6de0
fosicata1996 <fosalaucatalin@gmail.com>
2021-01-26 12:22:33 +0200

    06. Angular & Spring - Routing - Search products by name

--- p03-angular-ecommerce/src/app/components/product-list/product-list.component.ts
+++ p03-angular-ecommerce/src/app/components/product-list/product-list.component.ts
@@ -13,6 +13,7 @@ export class ProductListComponent implements OnInit
 
   products: Product[];
   currentCategoryId: number;
+  searchMode: boolean;
 
   constructor(private productService: ProductService,
               private route: ActivatedRoute)
@@ -27,6 +28,20 @@ export class ProductListComponent implements OnInit
   }
 
   listProducts()
+  {
+    this.searchMode = this.route.snapshot.paramMap.has('keyword');
+
+    if (this.searchMode)
+    {
+      this.handleSearchProducts();
+    }
+    else
+    {
+      this.handleListProducts();
+    }
+  }
+
+  handleListProducts()
   {
     // check if "id" parameter is available
     if (this.route.snapshot.paramMap.has('id'))
@@ -47,4 +62,15 @@ export class ProductListComponent implements OnInit
     )
   }
 
+  private handleSearchProducts()
+  {
+    const theKeyword: string = this.route.snapshot.paramMap.get('keyword');
+
+    // now we search for the products using keyword
+    this.productService.searchProducts(theKeyword).subscribe(
+      data => {
+        this.products = data;
+      }
+    );
+  }
 }
