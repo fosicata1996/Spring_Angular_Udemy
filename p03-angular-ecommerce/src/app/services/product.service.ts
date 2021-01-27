2b6e59174e7d5e81769ff873060d3811d7baee74
fosicata1996 <fosalaucatalin@gmail.com>
2021-01-27 20:48:24 +0200

    06. Angular & Spring - Pagination

--- p03-angular-ecommerce/src/app/services/product.service.ts
+++ p03-angular-ecommerce/src/app/services/product.service.ts
@@ -34,6 +34,14 @@ export class ProductService
     return this.getProducts(searchUrl);
   }
 
+  getProductListPaginate(thePage: number, thePageSize: number, theCategoryId: number): Observable<GetResponseProducts>
+  {
+    const searchUrl = `${this.baseUrl}/search/findByCategoryId?id=${theCategoryId}`
+      + `&page=${thePage}&size=${thePageSize}`;
+
+    return this.httpClient.get<GetResponseProducts>(searchUrl);
+  }
+
   searchProducts(theKeyword: string): Observable<Product[]>
   {
     // need to build URL based on keyword
@@ -61,6 +69,12 @@ interface GetResponseProducts
 {
   _embedded: {
     products: Product[];
+  },
+  page: {
+    size: number,
+    totalElements: number,
+    totalPages: number,
+    number: number
   }
 }
 
