6f810c327f47b11feea40f25f01968e84a383de5
fosicata1996 <fosalaucatalin@gmail.com>
2021-01-27 23:25:42 +0200

    06. Angular & Spring - Shopping Cart Status - Add cart service

--- p03-angular-ecommerce/src/app/components/product-list/product-list.component.ts
+++ p03-angular-ecommerce/src/app/components/product-list/product-list.component.ts
@@ -2,6 +2,8 @@ import {Component, OnInit} from '@angular/core';
 import {ProductService} from "../../services/product.service";
 import {Product} from "../../common/product";
 import {ActivatedRoute} from "@angular/router";
+import {CartItem} from "../../common/cart-item";
+import {CartService} from "../../services/cart.service";
 
 @Component({
   selector: 'app-product-list',
@@ -24,6 +26,7 @@ export class ProductListComponent implements OnInit
   previousKeyword: string = null;
 
   constructor(private productService: ProductService,
+              private cartService: CartService,
               private route: ActivatedRoute)
   {
   }
@@ -119,6 +122,6 @@ export class ProductListComponent implements OnInit
   {
     console.log(`Adding to cart: ${currentProduct.name}, ${currentProduct.unitPrice}`);
 
-    // TODO... do the real work
+    this.cartService.addToCart(new CartItem(currentProduct));
   }
 }
