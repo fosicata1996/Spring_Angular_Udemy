6b23875b70eb500d4d712d16e2e37e9a9cfb96d7
fosicata1996 <fosalaucatalin@gmail.com>
2021-01-28 21:39:08 +0200

    06. Angular & Spring - Checkout form - Add all forms

--- p03-angular-ecommerce/src/app/components/checkout/checkout.component.ts
+++ p03-angular-ecommerce/src/app/components/checkout/checkout.component.ts
@@ -10,6 +10,9 @@ export class CheckoutComponent implements OnInit
 {
   checkoutFormGroup: FormGroup;
 
+  totalPrice: number = 0;
+  totalQuantity: number = 0;
+
   constructor(private formBuilder: FormBuilder)
   {
   }
@@ -21,6 +24,28 @@ export class CheckoutComponent implements OnInit
         firstName: [''],
         lastName: [''],
         email: ['']
+      }),
+      shippingAddress: this.formBuilder.group({
+        street: [''],
+        city: [''],
+        state: [''],
+        country: [''],
+        zipCode: ['']
+      }),
+      billingAddress: this.formBuilder.group({
+        street: [''],
+        city: [''],
+        state: [''],
+        country: [''],
+        zipCode: ['']
+      }),
+      creditCard: this.formBuilder.group({
+        cardType: [''],
+        nameOnCard: [''],
+        cardNumber: [''],
+        securityCode: [''],
+        expirationMonth: [''],
+        expirationYear: ['']
       })
     });
   }
@@ -32,4 +57,16 @@ export class CheckoutComponent implements OnInit
     console.log("The email address is " + this.checkoutFormGroup.get('customer').value.email);
   }
 
+  copyShippingAddressToBillingAddress(event)
+  {
+    if (event.target.checked)
+    {
+      this.checkoutFormGroup.controls.billingAddress
+        .setValue(this.checkoutFormGroup.controls.shippingAddress.value);
+    }
+    else
+    {
+      this.checkoutFormGroup.controls.billingAddress.reset();
+    }
+  }
 }
