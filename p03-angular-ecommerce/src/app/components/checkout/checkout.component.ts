891f935f774c512f6352551cf38c1108a6b678ac
fosicata1996 <fosalaucatalin@gmail.com>
2021-02-02 14:13:50 +0200

    06. Angular & Spring - Save order to database - Frontend

--- p03-angular-ecommerce/src/app/components/checkout/checkout.component.ts
+++ p03-angular-ecommerce/src/app/components/checkout/checkout.component.ts
@@ -5,6 +5,11 @@ import {Country} from "../../common/country";
 import {State} from "../../common/state";
 import {Luv2ShopValidators} from "../../validators/luv2-shop-validators";
 import {CartService} from "../../services/cart.service";
+import {CheckoutService} from "../../services/checkout.service";
+import {Router} from "@angular/router";
+import {Order} from "../../common/order";
+import {OrderItem} from "../../common/order-item";
+import {Purchase} from "../../common/purchase";
 
 @Component({
   selector: 'app-checkout',
@@ -28,7 +33,9 @@ export class CheckoutComponent implements OnInit
 
   constructor(private formBuilder: FormBuilder,
               private luv2ShopFormService: Luv2ShopFormService,
-              private cartService: CartService)
+              private cartService: CartService,
+              private checkoutService: CheckoutService,
+              private router: Router)
   {
   }
 
@@ -110,10 +117,62 @@ export class CheckoutComponent implements OnInit
     if (this.checkoutFormGroup.invalid)
     {
       this.checkoutFormGroup.markAllAsTouched();
+      return;
     }
 
-    console.log(this.checkoutFormGroup.get('customer').value);
-    console.log("The email address is " + this.checkoutFormGroup.get('customer').value.email);
+    // set up order
+    let order = new Order();
+    order.totalPrice = this.totalPrice;
+    order.totalQuantity = this.totalQuantity;
+
+    // get cart items
+    const cartItems = this.cartService.cartItems;
+
+    // create orderItems from cartItems
+    // --> long way
+    /*
+      let orderItems:OrderItem[]=[];
+      for (let i = 0; i < cartItems.length; i++)
+      {
+        orderItems[i] = new OrderItem(cartItems[i]);
+      }
+    */
+
+
+    // --> short way
+    let orderItems: OrderItem[] = cartItems.map(currentCartItem => new OrderItem(currentCartItem));
+
+    // set up purchase
+    let purchase = new Purchase();
+
+    // populate purchase
+    purchase.customer = this.checkoutFormGroup.controls['customer'].value;
+
+    purchase.shippingAddress = this.checkoutFormGroup.controls['shippingAddress'].value;
+    purchase.shippingAddress.state = JSON.parse(JSON.stringify(purchase.shippingAddress.state)).name;
+    purchase.shippingAddress.country = JSON.parse(JSON.stringify(purchase.shippingAddress.country)).name;
+
+    purchase.billingAddress = this.checkoutFormGroup.controls['billingAddress'].value;
+    purchase.billingAddress.state = JSON.parse(JSON.stringify(purchase.billingAddress.state)).name;
+    purchase.billingAddress.country = JSON.parse(JSON.stringify(purchase.billingAddress.country)).name;
+
+    purchase.order = order;
+    purchase.orderItems = orderItems;
+
+    // call REST API via the CheckoutService
+    this.checkoutService.placeOrder(purchase).subscribe({
+        next: response => {
+          alert(`Your order has been received.\nOrder tracking number: ${response.orderTrackingNumber}`);
+
+          // reset cart
+          this.resetCart();
+        },
+        error: err => {
+          alert(`There was an error: ${err.message}`);
+        }
+      }
+    );
+
   }
 
   copyShippingAddressToBillingAddress(event)
@@ -283,5 +342,19 @@ export class CheckoutComponent implements OnInit
     this.cartService.totalPrice.subscribe(
       totalPrice => this.totalPrice = totalPrice
     );
+  }
+
+  private resetCart()
+  {
+    // reset cart data
+    this.cartService.cartItems = [];
+    this.cartService.totalPrice.next(0);
+    this.cartService.totalQuantity.next(0);
+
+    // reset the form
+    this.checkoutFormGroup.reset();
+
+    // navigate back to the products page
+    this.router.navigateByUrl("/products");
   }
 }
